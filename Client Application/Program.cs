9f207eb05f45a0d7e938ad5f48ae9ca729343d61
Rian Sanchez <98133009+Askoot94@users.noreply.github.com>
2025-03-17 17:38:25 -0400

    Added Starting Point Projects
    This includes a server, client, and class library projects.

---
+++ Client Application/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+//Write the path to the file here according to your system
+string pathToFiles = "";
+
+
+Console.WriteLine("Client Starting...");
+
+//Initialize the client
+IPAddress Server = IPAddress.Parse("127.0.0.1");
+
+var ipEndpoint = new IPEndPoint(Server, 53000);
+
+using TcpClient clientConnection = new TcpClient();
+
+//--Flow Control
+    Console.WriteLine("Client Application Ready.\nPress enter to Start.");
+    Console.ReadLine();
+
+//bind and connect
+await clientConnection.ConnectAsync(ipEndpoint);
+
+//extract connection stream
+await using NetworkStream stream = clientConnection.GetStream();
+
+StreamReader? FileReader = File.OpenText(pathToFiles);
+
+if(FileReader != null)
+{
+    for (int i = 0; i <20; i++)
+    {
+        //Read line from file
+        string rawMessage = FileReader.ReadLine();
+
+        if (rawMessage != null)
+        {
+            //convert to a stream of pure bytes.
+            var encodedMessage = Encoding.UTF8.GetBytes(rawMessage);
+
+
+            //call the extracted network stream and send a byte-encoded message.
+            await stream.WriteAsync(encodedMessage);
+
+            Console.WriteLine($"Sent: {rawMessage}");
+        }
+
+        Thread.Sleep(1000); //Stop 1 second
+    }
+    FileReader.Close();
+}
+else
+{
+    Console.WriteLine("File Not Found. Ending Process");
+}
+
+    Console.WriteLine("Client Application Done.\nPress enter to Finish.");
+    Console.ReadLine();
+
+
+clientConnection.Close();
+clientConnection.Dispose();
---
+++ FlightData/FlightData.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace FlightData
+{
+    public class FlightData
+    {
+        public DateTime TimeStamp { get; set; }
+        
+        public float FuelLevel {  get; set; }
+
+        public FlightData() 
+        {
+            TimeStamp = DateTime.Now;
+            FuelLevel = 0;
+        }
+        public FlightData(DateTime date, float fuel)
+        {
+            TimeStamp = date;
+            FuelLevel = fuel;
+        }
+
+    }
+
+    public class FlightDataEncoder
+    {
+        //For encoding into bytes, just utilize built-in GetBytes() command.
+
+        //Converts data stream into string and passes to other flightdata funciton.
+        static public FlightData GetFlightData(byte[] bytes)
+        {
+            string data = Encoding.UTF8.GetString(bytes);
+
+            FlightData flight = GetFlightData(data);
+            
+            return flight;
+        }
+
+        //Deserializes the string into a FlightData Object
+        static public FlightData GetFlightData(string dataString)
+        {
+            //Turn the single string into an array of multiple strings.
+            //The removeemptyentries array will hopefully remove the trailing last ' ' in the files.
+            string[] seperated= dataString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            FlightData data = new FlightData();
+
+            if (seperated.Length == 3)
+            {
+                //If there are three seperated data lines, that means this is the initial line of data from the client.
+                //It is the starting point and initial fuel levels/date for the rest of that flight.
+                
+                //string converts
+                DateTime flightDate = DateTime.Parse(seperated[1]);
+                float fuelLevel = float.Parse(seperated[2]);
+
+                //Update Object
+                data.FuelLevel = fuelLevel;
+                data.TimeStamp = flightDate;                
+            }
+            else if (seperated.Length == 2)
+            {
+                //Two data strings indicate this is just a continuing flight data, not the initial flight plan.
+
+                //String convert
+                DateTime flightDate = DateTime.Parse(seperated[0]);
+                float fuel = float.Parse(seperated[1]);
+
+                data.FuelLevel = fuel;
+                data.TimeStamp = flightDate;
+            }
+            else
+            {
+                throw new Exception("Could not Extract Data");
+            }
+
+            return data;
+        }
+    }
+}
